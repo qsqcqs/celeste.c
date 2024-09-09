bf598d884434fd3f56d20f8433e385bc724c3ae5
qsqcqs <qsqcqs@gmail.com>
2024-09-08 21:19:24 -0700

    collision

--- celeste.c
+++ celeste.c
@@ -19,7 +19,7 @@ unsigned char b[1];
 //printf("\n");
 }
 
-char map[4000];
+char map[3600];
 struct inputs
 {
 	bool up;
@@ -37,14 +37,332 @@ struct inputs
 };
 struct player
 {
-	double playerx;
-	double playery;
+	double x;
+	double y;
 	bool grounded;
 	double xspeed;
 	double yspeed;
 
 };
-//add switch statements maybe
+die(){/*to do*/}
+
+int getblock(x,y)
+{
+	return map[x + 80 * y];
+}
+void pixrender(x,y){/*oh god please no*/
+/*
+characters for this shit
+^vpbBd89 for spikes
+X for blocks
+blue for blocks, white for spikes
+*/
+switch (getblock(x,2*y) + 8 * getblock(x,2*y+1))
+{
+	case 0:
+		printf(' ');
+		break;
+	case 1:
+		printf('x');
+		break;
+	case 2:
+		printf('n');
+		break;
+	case 3:
+		printf('v');
+		break;
+	case 4:
+		printf('d');
+		break;
+	case 5:
+		printf('b');
+		break;
+	case 6:
+		//lava not implemented
+		break;
+	case 7:
+		//ice not implemented
+		break;
+	case 8:
+		printf('*');
+		break;
+	case 9:
+		printf('X');
+		break;
+	case 10:
+		//bruh this is a spike pointing at a block
+		break;
+	case 11:
+		printf('V');
+		break;
+	case 12:
+		printf('d');
+		break;
+	case 13:
+		printf('b');
+		break;
+	case 14:
+		//lava not implemented
+		break;
+	case 15:
+		//ice not implemented
+		break;
+	case 16:
+			printf('^');
+		break;
+	case 17:
+			printf('A');
+		break;
+	case 18:
+		printf('M');
+		break;
+	case 19:
+		printf('N');
+		break;
+	case 20://20 is fine-ish, 21 is iffier
+		printf('&');
+		break;
+	case 21:
+		printf('&');
+		break;
+	case 22:
+		//lava not implemented
+		break;
+	case 23:
+		//ice not implemented
+		break;
+	case 24:
+		printf('Y');
+		break;
+	case 25:
+		//spike into a block again?
+		break;
+	case 26: //26 and 27 are such a compromise
+		printf('<');
+		break;
+	case 27:
+		printf('<');
+		break;
+	case 28:
+		printf('<');
+		break;
+	case 29:
+		printf('Z');
+		break;
+	case 30:
+		//lava not implemented
+		break;
+	case 31:
+		//ice not implemented
+		break;
+	case 32:
+		printf('9');
+		break;
+	case 33:
+		printf('9');
+		break;
+	case 34://more compromises for 34 35
+		printf('<');
+		break;
+	case 35:
+		printf('<');
+		break;
+	case 36:
+		printf('<');
+		break;
+	case 37:
+		printf('Z');
+		break;
+	case 38:
+		//lava not implemented
+		break;
+	case 39:
+		//ice not implemented
+		break;
+	case 40:
+		printf('p');
+		break;
+	case 41:
+		printf('p');
+		break;
+	case 42://compromises for 42 43
+		printf('>');
+		break;
+	case 43:
+		printf('>');
+		break;
+	case 44:
+		printf('Z');
+		break;
+	case 45:
+		printf('>');
+		break;
+		//a lot of lava and ice not implemented
+	case 46:
+		break;
+	case 47:
+		break;
+	case 48:
+		break;
+	case 49:
+		break;
+	case 50:
+		break;
+	case 51:
+		break;
+	case 52:
+		break;
+	case 53:
+		break;
+	case 54:
+		break;
+	case 55:
+		break;
+	case 56:
+		break;
+	case 57:
+		break;
+	case 58:
+		break;
+	case 59:
+		break;
+	case 60:
+		break;
+	case 61:
+		break;
+	case 62:
+		break;
+	case 63:
+		break;
+};
+
+
+}
+render(){}
+void collide(struct player* maddyptr)
+{
+	/*
+	0->empty
+	1->wall
+	2->spikeup
+	3->spikedown
+	4->spikeleft
+	5->spikeright
+	6->lava
+	7->ice
+	*/
+	int leftaligint = (int)(maddyptr->x);
+	int heightalignint = (int)(maddyptr->y);
+	double xoffset = fmod(maddyptr->x,1)-.5;
+	double yoffset = fmod(maddyptr->y,1)-.5;
+	int xalignment = 0;
+	int yalignment = 0;
+	switch (getblock(leftaligint,heightalignint))
+	{
+		case 0:
+			break;
+		case 1:
+			die();
+			break;
+		case 2:
+			if (maddyptr->yspeed <= 0)
+			{
+				die();
+			}
+			break;
+		case 3:
+			if (maddyptr->yspeed >= 0)
+			{
+				die();
+			}
+			break;
+		case 4:
+			if (maddyptr->xspeed >= 0)
+			{
+				die();
+			}
+			break;
+		case 5:
+			if (maddyptr->xspeed <= 0)
+			{
+				die();
+			}
+			break;
+		case 6:
+			//to do
+			break;
+		case 7:
+			//to do
+			break;
+	}
+	if (yoffset > 0)
+	{
+		yalignment += 1;
+	}
+	else
+	{
+		if (yoffset < 0)
+		{
+			yalignment -= 1;
+		}
+	}
+	if (xoffset > 0)
+	{
+		xalignment += 1;
+	}
+	else
+	{
+		if (xoffset < 0)
+		{
+			xalignment -= 1;
+		}
+	}
+
+
+	//lazy spike detection is intentional (totally the truth ok)
+	
+	switch (yalignment)
+	{
+	case -1:
+		if (getblock(leftaligint,heightalignint-1) == 1)
+		{
+			maddyptr->y += yoffset;
+			maddyptr->yspeed = 0;
+		}
+		break;
+	case 0:
+		break;
+	case 1:
+		if (getblock(leftaligint,heightalignint+1) == 1)
+		{
+			maddyptr->y -= yoffset;
+			maddyptr->yspeed = 0;
+		}
+		break;
+	}
+	switch (xalignment)
+	{
+	case -1:
+		if (getblock(leftaligint-1,heightalignint) == 1)
+		{
+			maddyptr->x += xoffset;
+			maddyptr->xspeed = 0;
+		}
+		break;
+	case 0:
+		break;
+	case 1:
+		if (getblock(leftaligint+1,heightalignint) == 1)
+		{
+			maddyptr->x -= xoffset;
+			maddyptr->xspeed = 0;
+		}
+		break;
+	}
+
+
+	
+}
 char xdir(struct inputs* inputptr)
 {
 	if (inputptr->prevxdir == -1)
@@ -368,6 +686,8 @@ void movement(struct inputs* inputptr,struct player* maddieptr)
 	{
 		inputptr->prevdash = false;
 	}
+	maddieptr->x += maddieptr->xspeed;
+	maddieptr->y += maddieptr->yspeed;
 
 }
 
@@ -399,8 +719,8 @@ void game()
 
 	bool run;
 	struct player maddy;
-	maddy.playerx=0;
-	maddy.playery = 0;
+	maddy.x=0;
+	maddy.y = 0;
 	maddy.grounded = true;
 	maddy.xspeed = 0;
 	maddy.yspeed = 0;
